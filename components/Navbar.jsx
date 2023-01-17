3a47536f3dc53b22707247e9964d1164599c2376
urgt <urinbaevg@gmail.com>
2023-01-17 12:02:29 +0500

    small

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,29 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import styles from '../styles';
+
+import { navVariants } from '../utils/motion';
+
 const Navbar = () => (
-  <nav>
-    navbar vf
-  </nav>
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <img
+        src="/search.svg"
+        alt="search"
+        className="w-[24px] h-[24px]"
+      />
+      <h2 className="font-extrabold text-[24px] text-white">
+        METAVERSUS
+      </h2>
+    </div>
+  </motion.nav>
 );
 
 export default Navbar;
